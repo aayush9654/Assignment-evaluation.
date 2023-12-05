aa51e040f9c4bb8ca4f1d44bcbf448ca9d8eb74b
Ayush Baral <115476769+aayush9654@users.noreply.github.com>
2023-12-05 17:36:38 +0545

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science.
+# - Introduce commonly used libraries in data science.
+# - Provide examples of evaluating arithmetic expressions in Python.
+# - Demonstrate creating Markdown cells in Jupyter Lab.
+# - Showcase basic operations in a Jupyter Notebook.
+# 
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
